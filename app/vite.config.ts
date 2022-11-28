0a40fe06692085013e1d505f2e16a6c855cba032
Agustín Covarrubias <agucova@uc.cl>
2022-11-28 19:00:59 +0000

    dev(app): add vite plugins

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import eslintPlugin from '@nabla/vite-plugin-eslint';
+import svgrPlugin from 'vite-plugin-svgr';
+
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react(), eslintPlugin(), svgrPlugin()]
 })
