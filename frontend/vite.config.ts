735ace3451ff7afab1a839da5e2d21f8ef68bc3c
Agustín Covarrubias <agucova@uc.cl>
2022-11-28 20:54:30 +0000

    dev(frontend): add proxy to back

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,5 +5,15 @@ import svgrPlugin from 'vite-plugin-svgr'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    port: 3000,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8000',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
   plugins: [react(), eslintPlugin(), svgrPlugin()]
 })
