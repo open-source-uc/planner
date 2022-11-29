4a5fc0eafbae0a80ef70ac9a3f300d76759fd019
Agustín Covarrubias <agucova@uc.cl>
2022-11-29 00:10:55 +0000

    dev: cleanup, support arm64 on prisma

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,27 @@
+import asyncio
+
 from fastapi import FastAPI
+from prisma import Prisma
 
 app = FastAPI()
+prisma = Prisma()
+
+@app.on_event("startup")
+async def startup():
+    await prisma.connect()
+
+@app.on_event("shutdown")
+async def shutdown():
+    await prisma.disconnect()
 
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app)
+    asyncio.run(main())
