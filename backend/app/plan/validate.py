6cc746be308a61394a478dbcd994c74581a49f2a
Franco Giannoni <franco.giannoni.h@gmail.com>
2022-12-16 07:42:10 +0000

    fix: validate module bug with ReqLevel

--- backend/app/plan/validate.py
+++ backend/app/plan/validate.py
@@ -148,7 +148,7 @@ def is_satisfied(ctx: PlanContext, cl: Class, expr: Expr) -> bool:
             # Should planner reject guests with courses that have level restrictions?
             return False
         # TODO: Is this a `>=` relationship or actually an `=` relationship?
-        return ctx.plan.level.value >= expr.min_level
+        return ctx.plan.level.value >= expr.min_level.value
     if isinstance(expr, ReqSchool):
         return (ctx.plan.school == expr.school) == expr.equal
     if isinstance(expr, ReqProgram):
