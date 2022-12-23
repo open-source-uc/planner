5a35604d44caa1eac219f0550d182f9881f9c884
diegothx <diegothx@live.com>
2022-12-23 06:05:01 +0000

    refactor: update delet button from cards to new format

--- frontend/src/components/CourseCard.tsx
+++ frontend/src/components/CourseCard.tsx
@@ -1,20 +1,19 @@
-import { useState, useRef } from 'react'
+import { useRef } from 'react'
 import { useDrag, useDrop } from 'react-dnd'
 import type { Course } from '../lib/types'
 
-const CourseCard = (props: { course: any, isDragging: Function, handleMove: Function }): JSX.Element => {
-  const [course] = useState(props.course)
+const CourseCard = ({ course, isDragging, handleMove, remCourse }: { course: any, isDragging: Function, handleMove: Function, remCourse: Function }): JSX.Element => {
   const ref = useRef(null)
   const [collected = { isDragging: false }, drag] = useDrag(
     () => ({
       type: 'card',
 
       item: () => {
-        props.isDragging(true)
+        isDragging(true)
         return { course }
       },
       end () {
-        props.isDragging(false)
+        isDragging(false)
       },
       // call startMove when the drag starts
       collect (monitor) {
@@ -27,7 +26,7 @@ const CourseCard = (props: { course: any, isDragging: Function, handleMove: Func
   const [dropProps, drop] = useDrop(() => ({
     accept: 'card',
     drop (course: Course) {
-      props.handleMove(course)
+      handleMove(course)
     },
     collect: monitor => ({
       isOver: !!monitor.isOver(),
@@ -41,14 +40,16 @@ const CourseCard = (props: { course: any, isDragging: Function, handleMove: Func
     <div ref={ref} draggable={true} className={`px-2 ${!collected.isDragging ? 'pb-3' : ''}`}>
       {dropProps.isOver
         ? <div className={'bg-place-holder card'} />
-        : <>{!collected.isDragging && <div className={'bg-plan-comun card'}>
-        <div className='text-center'>{course.code}</div>
+        : <>{!collected.isDragging && <div className={'bg-plan-comun card relative group flex justify-center'}>
+          <button className='absolute top-0 right-2 hidden group-hover:inline' onClick={() => remCourse()}>x
+          </button>
+          <div className='flex items-center justify-center text-center'>{course.code}</div>
       </div>}
       </>}
     </div>
     {
       dropProps.isOver && <div className={'px-2 pb-3'}>
-      <div className={'bg-plan-comun card'}>
+      <div className={'bg-plan-comun card relative'}>
         <div className='text-center'>{course.code}</div>
       </div>
     </div>
--- frontend/src/pages/planner/PlanBoard.tsx
+++ frontend/src/pages/planner/PlanBoard.tsx
@@ -11,18 +11,17 @@ import CourseCard from '../../components/CourseCard'
  * TODO: Reemplazar este mockup temporal por algo de verdad.
  */
 const PlanBoard = ({ plan, setPlan }: { plan: string[][], setPlan: Function }): JSX.Element => {
-  // por alguna razon se buggea cuando inicia en estado false, asi que se inicia en true y se cambia a false cuando se termina de renderizar. Que asco
   const [isDragging, setIsDragging] = useState(false)
-  /*
   function remCourse (semIdx: number, code: string): void {
     const idx = plan[semIdx].indexOf(code)
     if (idx === -1) return
-    const newPlan = plan.slice()
-    newPlan[semIdx] = plan[semIdx].slice()
-    newPlan[semIdx].splice(idx, 1)
-    onPlanChange(newPlan)
+    setPlan((prev: string[][]) => {
+      const newPlan = [...prev]
+      newPlan[semIdx] = [...prev[semIdx]]
+      newPlan[semIdx].splice(idx, 1)
+      return newPlan
+    })
   }
- */
   function addCourse (semIdx: number): void {
     const courseCode = prompt('Course code?')
     if (courseCode == null || courseCode === '' || plan.flat().includes(courseCode.toUpperCase())) return
@@ -56,19 +55,6 @@ const PlanBoard = ({ plan, setPlan }: { plan: string[][], setPlan: Function }): 
       return newPlan
     })
   }
-  /* const semUi = plan.map((sem, semIdx) => {
-    const courses = sem.map(code => {
-      return (
-        <button key={code} className="w-20 h-10 bg-blue-300 text-center" onClick={() => remCourse(semIdx, code)}>{code}</button>
-      )
-    })
-    return (
-      <div key={semIdx} className="flex flex-col p-2 gap-2 bg-blue-400">
-        {courses}
-        <button key="+" className="w-20 h-10 bg-slate-300 text-center" onClick={() => addCourse(semIdx)}>+</button>
-      </div>
-    )
-  }) */
 
   /* const options: Array<[string, string[][]]> = [
     ['Resetear malla', []],
@@ -89,7 +75,7 @@ const PlanBoard = ({ plan, setPlan }: { plan: string[][], setPlan: Function }): 
         {plan.map((courses: string[], semester: number) => (
             <SemesterColumn key={semester} semester={semester} addEnd={({ course }: any) => moveCourse(semester, course, '')}>
               {courses?.map((code, index: number) => (
-                <CourseCard key={code} course={{ code, semester }} isDragging={(e: boolean) => setIsDragging(e)} handleMove={({ course }: any) => moveCourse(semester, course, code)} />
+                <CourseCard key={code} course={{ code, semester }} isDragging={(e: boolean) => setIsDragging(e)} handleMove={({ course }: any) => moveCourse(semester, course, code)} remCourse={() => remCourse(semester, code)}/>
               ))}
             {!isDragging && <button key="+" className="w-20 h-10 bg-slate-300 text-center" onClick={() => addCourse(semester)}>+</button>}
             </SemesterColumn>
