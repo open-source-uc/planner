b812d4ad09aa3ff459b3aee46a802d9a3157bfa8
Shantal Fabri s294042 <s294042@studenti.polito.it>
2023-01-05 18:41:20 +0100

    feat: roouting for user page

--- frontend/src/layout/Navbar.tsx
+++ frontend/src/layout/Navbar.tsx
@@ -9,6 +9,7 @@ function Navbar (): JSX.Element {
     <nav className="gap-4 font-bold mx-auto max-w-sm mt-5">
       <ul>
         <li className="inline mr-4"><Link to="/">Inicio</Link></li>
+        <li className="inline mr-4"><Link to="/user">User</Link></li>
         <li className="inline mr-4"><Link to="/planner">Planner</Link></li>
         {authState?.user == null ? (<li className="inline mr-4"><a href="/api/auth/login">Log in</a></li>) : (<li className="inline mr-4"><Link to="/logout">Cerrar sesión</Link></li>)}
       </ul>
