8182da73fcbcdf0b8b12426ad62eb32f08ab4772
Shantal Fabri <sfabri1@uc.cl>
2023-01-12 18:50:33 +0000

    feat: load plans and style changes

--- frontend/src/components/CurriculumList.tsx
+++ frontend/src/components/CurriculumList.tsx
@@ -1,6 +1,8 @@
 import CurriculumListRow from './CurriculumListRow'
 import plusIcon from '../assets/plus.svg'
 import { Link } from '@tanstack/react-router'
+import { useEffect, useState } from 'react'
+import { DefaultService } from '../client'
 
 //   const curriculums = [
 //     { id: 0, fav: true, name: 'Computación', creation: '10-01-2022', modified: '14-12-2022' },
@@ -10,19 +12,39 @@ import { Link } from '@tanstack/react-router'
 //   ]
 
 const CurriculumList = (): JSX.Element => {
+  const [plans, setPlans] = useState([])
+
+  useEffect(() => {
+    const readPlans = async (): Promise<void> => {
+      console.log('getting Plans ...')
+      const response = await DefaultService.readPlans()
+      setPlans(response)
+      console.log('data loaded')
+      console.log(plans)
+    }
+    readPlans().catch(err => {
+      console.log(err)
+    })
+  }, [])
+
+  function handleDelete (): void {
+    console.log('click')
+  }
+
   return (
-      <div className="flex items-center mb-4 h-full w-full"> {/* revisar si mejor con o sin items-center */}
+      <div className="flex  mb-4 h-full w-full"> {/* revisar si mejor con o sin items-center */}
           <div className=" bg-green-300 m-3 w-full">
                 <div className="flex space-x-4 items-center">
                     <h2 className="text-5xl font-normal leading-normal mt-0 mb-2 text-gray-800">Listado de Mallas</h2>
                     <Link to="/planner">
-                        <div className="flex flex-row items-center border">
-                            <button><img className="w-10 h-10" src={plusIcon} alt="Nueva Malla" /></button>  <p className="font-light">Nueva malla</p>
+                        <div className="hover-text">
+                            <button><img className="w-10 h-10" src={plusIcon} alt="Nueva Malla" /></button>
+                            <span className="tooltip-text">Crear Nueva Malla</span>
                         </div>
                     </Link>
                 </div>
 
-                <table className="table-auto text-center w-full p-3 "> {/* revisar si mejor con o sin h-full */}
+                <table className="table-auto text-center w-full p-3">
                     <tr className="border-b-4 border-gray-600">
                         <th></th>
                         <th>Nombre</th>
@@ -31,11 +53,16 @@ const CurriculumList = (): JSX.Element => {
                         <th>Acciones</th>
                     </tr>
 
-                    <CurriculumListRow curriculum={{ id: 0, fav: true, name: 'Computación', creation: '10-01-2022', modified: '14-12-2022' }}/>
-                    <CurriculumListRow curriculum={{ id: 1, fav: false, name: 'Diseno v2 esta si', creation: '10-03-2022', modified: '21-10-2022' }}/>
-                    <CurriculumListRow curriculum={{ id: 2, fav: false, name: 'Diseno', creation: '10-10-2020', modified: '10-10-2020' }}/>
-                    <CurriculumListRow curriculum={{ id: 3, fav: false, name: 'No seee', creation: '10-02-2020', modified: '14-12-2020' }}/>
+                    {plans?.map((plan) => {
+                      return (
+                            <CurriculumListRow key={1} handleDelete={handleDelete} curriculum={plan}/>
+                      )
+                    })}
 
+                    <CurriculumListRow handleDelete={handleDelete} curriculum={{ id: 0, fav: true, name: 'Computación', creation: '10-01-2022', modified: '14-12-2022' }}/>
+                    <CurriculumListRow handleDelete={handleDelete} curriculum={{ id: 1, fav: false, name: 'Diseno v2 esta si', creation: '10-03-2022', modified: '21-10-2022' }}/>
+                    <CurriculumListRow handleDelete={handleDelete} curriculum={{ id: 2, fav: false, name: 'Diseno', creation: '10-10-2020', modified: '10-10-2020' }}/>
+                    <CurriculumListRow handleDelete={handleDelete} curriculum={{ id: 3, fav: false, name: 'No seee', creation: '10-02-2020', modified: '14-12-2020' }}/>
                 </table>
           </div>
       </div>
--- frontend/src/components/CurriculumListRow.tsx
+++ frontend/src/components/CurriculumListRow.tsx
@@ -1,13 +1,13 @@
 import { Link } from '@tanstack/react-router'
 
-const CurriculumListRow = ({ curriculum }: { curriculum: { id: number, fav: boolean, name: string, creation: string, modified: string } }): JSX.Element => {
+const CurriculumListRow = ({ handleDelete, curriculum }: { handleDelete: Function, curriculum: { id: number, fav: boolean, name: string, creation: string, modified: string } }): JSX.Element => {
   return (
         <tr>
             { curriculum.fav ? <td>★</td> : <td>☆</td>}
             <td><Link to="/planner">{curriculum.name}</Link></td>
             <td>{curriculum.creation}</td>
             <td>{curriculum.modified}</td>
-            <td>edit delete duplicate</td>
+            <td><div className='space-x-4 items-center'><button className='text-blue-600'>Editar</button> <button className='text-red-600' onClick={() => handleDelete()}>Eliminar</button></div></td>
         </tr>
   )
 }
