378ae22c2707f3d35f3942879b97744b19d0edd2
Diegothx <diegothx@live.com>
2023-01-14 02:31:04 +0000

    fix: update client

--- frontend/src/client/index.ts
+++ frontend/src/client/index.ts
@@ -9,7 +9,6 @@ export type { OpenAPIConfig } from './core/OpenAPI';
 export type { Diagnostic } from './models/Diagnostic';
 export type { HTTPValidationError } from './models/HTTPValidationError';
 export { Level } from './models/Level';
-export type { PostCreateInput } from './models/PostCreateInput';
 export type { ValidatablePlan } from './models/ValidatablePlan';
 export type { ValidationError } from './models/ValidationError';
 export type { ValidationResult } from './models/ValidationResult';
