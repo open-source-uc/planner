1cc612f2d732d3460b4dc041846e1d5f7202eca0
Shantal Fabri <sfabri1@uc.cl>
2023-01-16 13:49:12 +0000

    style: remove bg colors

--- frontend/src/components/UserInfo.tsx
+++ frontend/src/components/UserInfo.tsx
@@ -2,7 +2,7 @@ const UserInfo = (): JSX.Element => {
   return (
     <>
         <div className="flex flex-col mb-4 h-full">
-            <div className="h-1/2 bg-green-300 m-3">
+            <div className="h-1/2 m-3">
                 <h3 className="text-4xl font-normal leading-normal mt-0 mb-2 text-gray-800">Información Personal</h3>
                 <ul className="m-3 space-y-2">
                     <li>Nombre: Pepe Perez</li>
@@ -11,7 +11,7 @@ const UserInfo = (): JSX.Element => {
                 </ul>
             </div>
             <hr />
-            <div className="h-1/2 bg-cyan-200 m-3">
+            <div className="h-1/2 m-3">
                 <h3 className="text-4xl font-normal leading-normal mt-0 mb-2 text-gray-800">Información Académica (Según SIDING)</h3>
                 <ul className="m-3 space-y-2">
                     <li>Título: No Inscrito</li>
