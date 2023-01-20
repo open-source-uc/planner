ec436e04de0b980ee9fb908c6545147bf3e79655
Shantal Fabri <sfabri1@uc.cl>
2023-01-20 13:28:04 +0000

    style: pequeños cambios de colores

--- frontend/src/layout/Navbar.tsx
+++ frontend/src/layout/Navbar.tsx
@@ -6,23 +6,25 @@ function Navbar (): JSX.Element {
   const authState = useAuth()
 
   return (
-    <nav className="gap-4 font-bold mx-5 my-3 flex justify-between items-center">
-      <ul className="flex items-center">
-        <li className="inline mr-4"><Link to="/">Inicio</Link></li>
-      </ul>
-      <ul className="flex items-center">
-        <li className="inline mr-4 justify-end"><Link to="/planner">Crear Malla</Link></li>
-        <li className="inline mr-4 justify-end">
-        {authState?.user == null
-          ? (<a href="/api/auth/login">Log in</a>)
-          : (<>
-            <li className="inline mr-4"><Link to="/user">User</Link></li>
-            <Link to="/logout">Cerrar sesión</Link>
-          </>)}
-        </li>
-      </ul>
-
-    </nav>)
+    <div className='bg-blue-300'>
+      <nav className="gap-4 font-bold mx-5 my-3 flex justify-between items-center">
+        <ul className="flex items-center">
+          <li className="inline mr-4"><Link to="/">Inicio</Link></li>
+        </ul>
+        <ul className="flex items-center">
+          <li className="inline mr-4 justify-end"><Link to="/planner">Crear Malla</Link></li>
+          <li className="inline mr-4 justify-end">
+          {authState?.user == null
+            ? (<a href="/api/auth/login">Log in</a>)
+            : (<>
+              <li className="inline mr-4"><Link to="/user">User</Link></li>
+              <Link to="/logout">Cerrar sesión</Link>
+            </>)}
+          </li>
+        </ul>
+      </nav>
+    </div>
+  )
 }
 
 export default Navbar
