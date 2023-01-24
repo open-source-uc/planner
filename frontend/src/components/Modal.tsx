0785a55f0f621e48bf6329e5d5a8cf6eba8c9112
Diegothx <diegothx@live.com>
2023-01-24 02:08:14 +0000

    feat: infinite scroll to selector

--- frontend/src/components/Modal.tsx
+++ frontend/src/components/Modal.tsx
@@ -5,30 +5,38 @@ import info from '../assets/info.svg'
 
 const MyDialog = ({ equivalence, open, onClose }: { equivalence?: Equivalence, open: boolean, onClose: Function }): JSX.Element => {
   const [courses, setCourses] = useState<Course[]>([])
+  const [offset, setOffset] = useState(10)
   const [selectedCourse, setSelectedCourse] = useState('')
 
-  const previousClasses = useRef('')
-  async function getCourseDetails (equivalence: Equivalence): Promise<void> {
-    const response = await DefaultService.getCourseDetails(equivalence.courses)
-    setCourses(response)
-    previousClasses.current = equivalence.code
+  const tbodyRef = useRef<HTMLTableSectionElement>(null)
+
+  async function getCourseDetails (coursesCodes: string[]): Promise<void> {
+    const response = await DefaultService.getCourseDetails(coursesCodes)
+    setCourses([...courses, ...response])
+  }
+
+  const handleScroll: React.EventHandler<React.SyntheticEvent<HTMLTableSectionElement>> = event => {
+    const { scrollTop, scrollHeight, clientHeight } = event.currentTarget
+    if (scrollTop + clientHeight === scrollHeight && equivalence !== undefined) {
+      setOffset(offset + 10)
+      getCourseDetails(equivalence.courses.slice(offset, offset + 10)).catch(err => console.log(err))
+    }
   }
 
   useEffect(() => {
-    if (!open)setCourses([])
-    else if (equivalence !== undefined) getCourseDetails(equivalence).catch(err => console.log(err))
+    if (!open) { setCourses([]); setOffset(10) } else if (equivalence !== undefined) getCourseDetails(equivalence.courses.slice(0, 10)).catch(err => console.log(err))
   }, [open])
-  console.log(courses)
+
   if (equivalence === undefined) return <></>
   return (
-    <Dialog as="div" className="relative z-10" open={open} onClose={() => onClose()}>
-        <div className="fixed inset-0 overflow-y-auto">
+    <Dialog as="div" className="relative z-10" open={open} onClose={() => { if (tbodyRef.current != null) onClose() }}>
+        <div className="fixed inset-0 overflow-y-auto ">
             <div className="flex items-center min-h-screen justify-center p-4 text-center">
-                <Dialog.Panel className="w-full overflow-y-hidden max-w-4xl transform  rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
+                <Dialog.Panel className=" w-11/12 bg-blue-50 overflow-y-hidden max-w-4xl transform  rounded-2xl border border-indigo-900 p-6 text-left align-middle shadow-xl transition-all">
                     <Dialog.Title as="h3" className="text-lg font-medium leading-6 text-gray-900">
                         {equivalence.name}
                     </Dialog.Title>
-                    <table className="text-left table-auto w-11/12 p-3 m-auto">
+                    <table className="text-left table-auto mb-3 p-3 mx-3 w-full">
                         <thead className="w-full">
                             <tr className="border-b-2 border-gray-600 flex w-full">
                                 <th className="w-8"></th>
@@ -39,7 +47,7 @@ const MyDialog = ({ equivalence, open, onClose }: { equivalence?: Equivalence, o
                                 <th className="w-8"></th>
                             </tr>
                         </thead>
-                        <tbody className="bg-grey-light flex flex-col items-center justify-between overflow-y-scroll w-full max-h-96">
+                        <tbody onScroll={handleScroll} className="bg-white flex flex-col items-center justify-between overflow-y-scroll w-full max-h-72 pt-2">
                         {courses.map((course: Course) => (
                             <tr key={course.code} className="flex w-full mb-3">
                                 <td className="w-8"><input className='ml-1' id={course.code} type="radio" name="status" value={course.code} onChange={e => setSelectedCourse(e.target.value)}/></td>
@@ -51,9 +59,11 @@ const MyDialog = ({ equivalence, open, onClose }: { equivalence?: Equivalence, o
                             </tr>
                         ))}</tbody>
                     </table>
-
-                    <button onClick={() => onClose()}>Cancelar</button>
-                    <button onClick={() => onClose(selectedCourse)}>Guardar</button>
+                    <div className='right-0'>{courses.length} - {equivalence.courses.length}</div>
+                    <div className='float-right mx-3 '>
+                        <button className="btn mr-2" onClick={() => onClose()}>Cancelar</button>
+                        <button className="btn " onClick={() => onClose(selectedCourse)}>Guardar</button>
+                    </div>
                 </Dialog.Panel>
                 </div>
                 </div>
