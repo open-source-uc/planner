c6e1fb31ab9aa825b845316d474ea3a55cfd3b45
Shantal Fabri <sfabri1@uc.cl>
2023-01-25 23:53:34 +0000

    feat: basic home-page layout

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,16 +1,23 @@
 import { useAuth } from '../contexts/auth.context'
+import demo from '../assets/demo.png'
+import demoGif from '../assets/demo_gif.gif'
 
 const Home = (): JSX.Element => {
   const authState = useAuth()
 
   return (
-        <div className="max-w-md mx-auto mt-8 prose">
-            <h2>Inicio</h2>
-            {authState?.user != null && (<p>Has iniciado sesión.</p>)}
-            <pre>
-                {authState != null && JSON.stringify(authState, null, 2)}
-            </pre>
-        </div>
+    <div className='bg-gradient-to-b from-blue-600 h-full'>
+      <div className="max-w-xl mx-auto mt-8 prose cente">
+          <h2 className='text-white'>Bienvenido a PanguiPath, el lugar donde puedes planificar tu carrera universitaria.</h2>
+          {/* {authState?.user != null && (<p>Has iniciado sesión.</p>)} */}
+          {authState?.user != null ? (<p>Has iniciado sesión.</p>) : (<p>iniciar sesión</p>)}
+          {/* <pre>
+              {authState != null && JSON.stringify(authState, null, 2)}
+          </pre> */}
+      </div>
+      <img className="w-1/2 m-5 mx-auto" alt="demo" src={demoGif}></img>
+    </div>
+
   )
 }
 
