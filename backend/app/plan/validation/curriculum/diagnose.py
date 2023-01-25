c853fba02d36b2a2d282ffdebd7ec72aaf30bce1
Franco Giannoni <franco.giannoni.h@gmail.com>
2023-01-25 18:45:46 +0000

    refactor: last change in curriculum err msg

--- backend/app/plan/validation/curriculum/diagnose.py
+++ backend/app/plan/validation/curriculum/diagnose.py
@@ -10,10 +10,8 @@ class CurriculumErr(DiagnosticErr):
     missing: str
 
     def message(self) -> str:
-        return f"""
-        Se deben completar los créditos de '{self.superblock}'.
-        Falta lo siguiente: {self.missing}
-        """
+        return f"""Se deben completar los créditos de '{self.superblock}'.
+        Falta lo siguiente: {self.missing}"""
 
 
 def _diagnose_block(out: ValidationResult, node: SolvedNode):
