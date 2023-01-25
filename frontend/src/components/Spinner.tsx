c4eb282237fe577eb9057ca73c29b0e99560e4ed
Agustín Covarrubias <agucova@uc.cl>
2023-01-25 10:40:01 +0000

    fix: make spinner actually spin

--- frontend/src/components/Spinner.tsx
+++ frontend/src/components/Spinner.tsx
@@ -1,9 +1,13 @@
-export function Spinner (): JSX.Element {
-  return <div className='mx-auto my-auto w-20 h-10'>
-            <div className="flex justify-center items-center mb-4">
-              <div className="spinner-border animate-spin inline-block w-8 h-8 border-4 rounded-full" role="status">
-              </div>
-            </div>
-            <p>Cargando...</p>
+interface SpinnerProps {
+  message: string
+}
+
+export function Spinner ({ message }: SpinnerProps): JSX.Element {
+  return <div className='mx-auto my-auto max-w-20 max-h-10'>
+       <svg className="animate-spin h-10 w-10 mx-auto mb-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+        <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
+        <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+      </svg>
+            <p className="">{message}</p>
           </div>
 }
