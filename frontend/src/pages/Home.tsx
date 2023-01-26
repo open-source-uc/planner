6a8b44ca89848448f129ab6471e088e45f0ad191
Shantal Fabri <sfabri1@uc.cl>
2023-01-26 00:51:40 +0000

    feat: add buttons

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,3 +1,4 @@
+import { Link } from '@tanstack/react-router'
 import { useAuth } from '../contexts/auth.context'
 import demo from '../assets/demo.png'
 import demoGif from '../assets/demo_gif.gif'
@@ -6,16 +7,17 @@ const Home = (): JSX.Element => {
   const authState = useAuth()
 
   return (
-    <div className='bg-gradient-to-b from-blue-600 h-full'>
-      <div className="max-w-xl mx-auto mt-8 prose cente">
-          <h2 className='text-white'>Bienvenido a PanguiPath, el lugar donde puedes planificar tu carrera universitaria.</h2>
-          {/* {authState?.user != null && (<p>Has iniciado sesión.</p>)} */}
-          {authState?.user != null ? (<p>Has iniciado sesión.</p>) : (<p>iniciar sesión</p>)}
+    <div className='bg-gradient-to-b from-blue-500 h-full'>
+      <div className="max-w-3xl mx-auto mt-8 prose center">
+          <h1 className='text-white text-center font-medium'>Bienvenido a PanguiPath, el lugar donde puedes planificar tu carrera universitaria.</h1>
+          {authState?.user != null
+            ? (<div className='flex justify-evenly'><Link to="/user"><button className='rounded-lg py-2 px-4 bg-blue-800 text-white'>Mis mallas</button></Link></div>)
+            : (<div className='flex justify-evenly'><a href="/api/auth/login"><button className='rounded-lg py-2 px-4 bg-blue-800 text-white'>Iniciar sesión</button></a><Link to="/planner"><button className='rounded-lg py-2 px-4 bg-blue-800 text-white'>Continuar como invitado</button></Link></div>)}
           {/* <pre>
               {authState != null && JSON.stringify(authState, null, 2)}
           </pre> */}
       </div>
-      <img className="w-1/2 m-5 mx-auto" alt="demo" src={demoGif}></img>
+      <img className="w-auto m-5 mx-auto h-3/5" alt="demo" src={demoGif}></img>
     </div>
 
   )
