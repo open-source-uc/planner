6c9591f9ec6f705aeb4a2e44a52ed513d27488a1
Shantal Fabri <sfabri1@uc.cl>
2023-01-26 01:13:57 +0000

    style: tooltip nueva malla y titulo

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,6 +1,5 @@
 import { Link } from '@tanstack/react-router'
 import { useAuth } from '../contexts/auth.context'
-import demo from '../assets/demo.png'
 import demoGif from '../assets/demo_gif.gif'
 
 const Home = (): JSX.Element => {
@@ -9,7 +8,7 @@ const Home = (): JSX.Element => {
   return (
     <div className='bg-gradient-to-b from-blue-500 h-full'>
       <div className="max-w-3xl mx-auto mt-8 prose center">
-          <h1 className='text-white text-center font-medium'>Bienvenido a PanguiPath, el lugar donde puedes planificar tu carrera universitaria.</h1>
+          <h1 className='text-white text-center font-medium'>Bienvenido a PanguiPath, el lugar donde puedes planificar tu carrera universitaria</h1>
           {authState?.user != null
             ? (<div className='flex justify-evenly'><Link to="/user"><button className='rounded-lg py-2 px-4 bg-blue-800 text-white'>Mis mallas</button></Link></div>)
             : (<div className='flex justify-evenly'><a href="/api/auth/login"><button className='rounded-lg py-2 px-4 bg-blue-800 text-white'>Iniciar sesión</button></a><Link to="/planner"><button className='rounded-lg py-2 px-4 bg-blue-800 text-white'>Continuar como invitado</button></Link></div>)}
@@ -19,7 +18,6 @@ const Home = (): JSX.Element => {
       </div>
       <img className="w-auto m-5 mx-auto h-3/5" alt="demo" src={demoGif}></img>
     </div>
-
   )
 }
 
