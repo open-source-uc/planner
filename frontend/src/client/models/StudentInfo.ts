bcd8557317c50bba40b7509c2f952d18a0b86e72
kovaxis <martin.andrigheti@gmail.com>
2023-04-26 02:41:51 +0000

    chore: generate client for new field

--- frontend/src/client/models/StudentInfo.ts
+++ frontend/src/client/models/StudentInfo.ts
@@ -5,6 +5,7 @@
 export type StudentInfo = {
     full_name: string;
     cyear: string;
+    is_cyear_supported: boolean;
     admission: Array<any>;
     reported_major?: string;
     reported_minor?: string;
