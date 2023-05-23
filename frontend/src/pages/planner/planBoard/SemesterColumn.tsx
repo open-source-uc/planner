0bdeece2fe8b0da2faf9c5f0320621cf6a01d44f
Shantal Fabri s294042 <s294042@studenti.polito.it>
2023-05-23 16:23:45 +0000

    feat: detect passed semesters

--- frontend/src/pages/planner/planBoard/SemesterColumn.tsx
+++ frontend/src/pages/planner/planBoard/SemesterColumn.tsx
@@ -1,5 +1,6 @@
 
 import { useDrop } from 'react-dnd'
+import { useAuth } from '../../../contexts/auth.context'
 import { Course } from '../../../client'
 
 interface SemesterColumnProps {
@@ -9,6 +10,8 @@ interface SemesterColumnProps {
 }
 
 const SemesterColumn = ({ semester, addEnd, children }: SemesterColumnProps): JSX.Element => {
+  const authState = useAuth()
+
   const [dropProps, drop] = useDrop(() => ({
     accept: 'card',
     drop (course: Course & { semester: number }) {
@@ -19,18 +22,34 @@ const SemesterColumn = ({ semester, addEnd, children }: SemesterColumnProps): JS
     })
   }))
   return (
-    <div className={'drop-shadow-xl w-[165px] shrink-0 bg-base-200 rounded-lg h-full flex flex-col'}>
-      <h2 className="mt-1 text-[1.2rem] text-center">{`Semestre ${semester}`}</h2>
-      <div className="my-2 divider"></div>
-      <div className={'max-h-full '}>
-        {children}
-      </div>
-      <div ref={drop} className={'px-2 flex min-h-[90px] flex-grow'}>
-        {dropProps.isOver &&
-            <div className={'bg-place-holder card w-full'} />
-        }
-      </div>
-    </div>
+    <>
+    {((authState?.passed?.length) != null) && (semester <= authState?.passed?.length)
+      ? <div className={'drop-shadow-xl w-[165px] shrink-0 bg-base-200 rounded-lg h-full flex flex-col '}>
+        <h2 className="mt-1 text-[1.2rem] text-center">{`Semestre ${semester}`}</h2>
+        <div className="my-2 divider"></div>
+        <div className={'max-h-full bg-black'}>
+          {children}
+        </div>
+        <div ref={drop} className={'px-2 flex min-h-[90px] flex-grow'}>
+          {dropProps.isOver &&
+              <div className={'bg-place-holder card w-full'} />
+          }
+        </div>
+        </div>
+      : <div className={'drop-shadow-xl w-[165px] shrink-0 bg-base-200 rounded-lg h-full flex flex-col '}>
+        <h2 className="mt-1 text-[1.2rem] text-center">{`Semestre ${semester}`}</h2>
+        <div className="my-2 divider"></div>
+        <div className={'max-h-full '}>
+          {children}
+        </div>
+        <div ref={drop} className={'px-2 flex min-h-[90px] flex-grow'}>
+          {dropProps.isOver &&
+              <div className={'bg-place-holder card w-full'} />
+          }
+        </div>
+        </div>
+      }
+    </>
   )
 }
 
