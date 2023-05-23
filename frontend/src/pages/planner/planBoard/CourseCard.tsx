ea7c8d486d01475c576222af87781cf3869d2625
kovaxis <martin.andrigheti@gmail.com>
2023-05-23 04:56:35 +0000

    fix: ping animations

--- frontend/src/pages/planner/planBoard/CourseCard.tsx
+++ frontend/src/pages/planner/planBoard/CourseCard.tsx
@@ -166,10 +166,10 @@ const Card = ({ semester, index, courseBlock, cardData, hasEquivalence, openSele
       <div className='absolute bottom-2 left-2 text-[0.5rem] opacity-75'>{cardData.credits} créd.</div>
       {hasError && <span className="flex absolute h-3 w-3 top-0 right-0 -mt-1 -mr-1">
         <span className={`${allowAnimations ? 'animate-ping' : ''} absolute inline-flex h-full w-full rounded-full bg-red-300 opacity-90`}></span>
-        <span className={`${allowAnimations ? 'animate-ping' : ''} relative inline-flex rounded-full h-3 w-3 bg-red-400`}></span>
+        <span className="relative inline-flex rounded-full h-3 w-3 bg-red-400"></span>
       </span> }
       {!hasError && hasWarning && <span className="flex absolute h-3 w-3 top-0 right-0 -mt-1 -mr-1">
-        <span className="absolute inline-flex h-full w-full rounded-full bg-yellow-300 opacity-90"></span>
+        <span className={`${allowAnimations ? 'animate-ping' : ''} absolute inline-flex h-full w-full rounded-full bg-yellow-300 opacity-90`}></span>
         <span className="relative inline-flex rounded-full h-3 w-3 bg-yellow-400"></span>
       </span> }
   </div>
