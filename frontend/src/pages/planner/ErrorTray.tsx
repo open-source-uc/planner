831e39cd455f39039c5d0c41ec5c35803235801e
diegothx <diegothx@live.com>
2023-06-06 02:49:16 +0000

    style: small paddings resize on ErrorTray

--- frontend/src/pages/planner/ErrorTray.tsx
+++ frontend/src/pages/planner/ErrorTray.tsx
@@ -25,9 +25,9 @@ const Message = (diag: FlatDiagnostic, key: number, open: boolean): JSX.Element 
 
   return (
   <div key={key} className={`overflow-hidden flex p-3 text-sm rounded-lg border ${w ? 'text-yellow-700 border-yellow-300 bg-yellow-50' : 'text-red-800 border-red-300 bg-red-50'}`} role="alert">
-    <svg aria-hidden="true" className="flex-shrink-0 inline w-5 h-5 mr-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clipRule="evenodd"></path></svg>
+    <svg aria-hidden="true" className="flex-shrink-0 inline w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clipRule="evenodd"></path></svg>
     <span className="sr-only">Info</span>
-    <div className={`min-w-[14rem] ${open ? '' : 'hidden'} `}>
+    <div className={`min-w-[14rem] ml-2 ${open ? '' : 'hidden'} `}>
       <span className={'font-semibold '}>{`${w ? 'Advertencia' : 'Error'}: `}</span>
       {`${diag.message}`}
     </div>
@@ -52,8 +52,8 @@ const ErrorTray = ({ diagnostics, validating }: { diagnostics: FlatDiagnostic[],
   })
   const messageList: JSX.Element[] = diagnostics.map((diag, index) => Message(diag, index, open || hasError))
   return (
-    <div className={`h-[95%] flex relative border-slate-300 border-2 rounded-lg shadow-lg motion-reduce:transition-none transition-all ${hasError || open ? 'w-80 min-w-[20rem]' : 'w-20'}`}>
-      <div className='relative mb-2 overflow-y-auto h-full w-full py-4 rounded-lg  bg-slate-100 z-20 px-4'>
+    <div className={`h-[95%] flex relative border-slate-300 border-2 rounded-lg shadow-lg motion-reduce:transition-none transition-all ${hasError || open ? 'w-80 min-w-[20rem]' : 'w-[4.5rem]'}`}>
+      <div className='relative mb-2 overflow-y-auto h-full w-full py-4 rounded-lg  bg-slate-100 z-20 px-3'>
         <div className='flex mb-4 mx-3'>
           <div className='group relative flex'>
             <span className={`fixed z-10 transition-all motion-reduce:transition-none scale-0 ${hasError ? 'group-hover:scale-100' : ''}`}>
