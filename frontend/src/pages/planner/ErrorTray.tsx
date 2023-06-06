504a304bed5e301c22b24f78870291791b56b904
diegothx <diegothx@live.com>
2023-06-06 02:07:41 +0000

    style: change button icon and adjust spinner to closed ErrorTray

--- frontend/src/pages/planner/ErrorTray.tsx
+++ frontend/src/pages/planner/ErrorTray.tsx
@@ -5,12 +5,12 @@ import { Spinner } from '../../components/Spinner'
 /**
  * This is what is displayed when there are no errors or warnings.
  */
-const NoMessages = (): JSX.Element => {
+const NoMessages = ({ open }: { open: boolean }): JSX.Element => {
   return (
     <div className="flex p-4 mb-4 text-sm text-green-800 border border-green-300 rounded-lg bg-green-50 " role="alert">
       <svg aria-hidden="true" className="flex-shrink-0 inline w-5 h-5 mr-3" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clipRule="evenodd"></path></svg>
       <span className="sr-only">Info</span>
-      <div>
+      <div className={`min-w-[14rem] ${open ? '' : 'hidden'} `}>
         <span className='font-medium'>Felicitaciones!</span> No hay errores o advertencias.
       </div>
     </div>
@@ -52,27 +52,27 @@ const ErrorTray = ({ diagnostics, validating }: { diagnostics: FlatDiagnostic[],
   })
   const messageList: JSX.Element[] = diagnostics.map((diag, index) => Message(diag, index, open || hasError))
   return (
-    <div className="h-[95%] flex relative">
-      <div className={`h-16 absolute ${hasError ? 'translate-x-0' : '-translate-x-4'} z-10 motion-reduce:transition-none transition-all`}>
-        <button className="h-full bg-slate-100 rounded-l-lg border-slate-300 border-t-2 border-b-2 border-l-2 text-slate-500 focus:outline-none" onClick={() => setOpen(prev => !prev)}>
-            <svg className={`w-4 h-4 ${hasError || open ? '' : 'transform rotate-180'}`} viewBox="0 0 24 24" fill="currentColor">
-              <path d="M0 0h24v24H0z" fill="none" />
-              <path d="M8 5v14l11-7z" />
-            </svg>
-          </button>
-        </div>
-      <div className={` border-slate-300 border-2 rounded-lg shadow-lg motion-reduce:transition-none transition-all ${!hasError ? 'rounded-tl-none' : ''} ${hasError || open ? 'w-80 min-w-[200px]' : 'w-20'}`}>
-        <div className={`relative mb-2 overflow-y-auto h-full w-full py-4 rounded-lg  bg-slate-100 z-20 px-4 ${!hasError ? 'rounded-tl-none' : ''}`}>
-        <div className='flex mb-4 justify-center'>
-          <svg aria-hidden="true" className="flex-shrink-0 inline w-6 h-7" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-            <path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clipRule="evenodd"></path>
-          </svg>
-          {(hasError || open) && <p className="whitespace-nowrap ml-1 text-xl font-semibold text-center h-7 overflow-hidden">Errores y Advertencias </p>}
+    <div className={`h-[95%] flex relative border-slate-300 border-2 rounded-lg shadow-lg motion-reduce:transition-none transition-all ${hasError || open ? 'w-80 min-w-[20rem]' : 'w-20'}`}>
+      <div className='relative mb-2 overflow-y-auto h-full w-full py-4 rounded-lg  bg-slate-100 z-20 px-4'>
+        <div className='flex mb-4 mx-3'>
+          <div className='group relative flex'>
+            <span className={`fixed z-10 transition-all motion-reduce:transition-none scale-0 ${hasError ? 'group-hover:scale-100' : ''}`}>
+              <div className="absolute right-2.5 top-1 w-4 h-4 bg-gray-800 rotate-45 rounded" />
+              <span className={'absolute right-4 -top-1 w-48 z-10 rounded bg-gray-800 p-2 text-xs text-white'}>
+                Es necesario resolver todos los errores existentes para minimizar la bandeja de Errores y Advertencias.
+              </span>
+            </span>
+            <button className={`${hasError ? 'cursor-not-allowed stroke-slate-400' : 'stroke-current'}`} disabled={hasError} onClick={() => setOpen(prev => !prev)}>
+              <svg className="w-5 h-5 flex" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+                  <path d="M2 12h20M2 6h20M2 18h20"></path>
+              </svg>
+            </button>
+          </div>
+          {(hasError || open) && <p className="whitespace-nowrap ml-2 text-xl font-semibold text-center h-7 overflow-hidden">Errores y Advertencias </p>}
         </div>
         <div className="flex flex-col gap-2">
-          {validating ? <Spinner message='Validando...'/> : <>{messageList.length > 0 ? messageList : <NoMessages/>}</>}
+            {validating ? <Spinner message={`${hasError || open ? 'Validando...' : ''}`}/> : <>{messageList.length > 0 ? messageList : <NoMessages open={hasError || open}/>}</>}
         </div>
-      </div>
       </div>
     </div>
   )
