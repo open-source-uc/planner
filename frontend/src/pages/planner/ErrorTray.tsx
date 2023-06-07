6d8395caab859e96682973b9e0015773bed8fac1
Diegothx <diegothx@live.com>
2023-06-07 03:51:29 +0000

    style: handle scrollbar of chrome while ErrorTray is minimize

--- frontend/src/pages/planner/ErrorTray.tsx
+++ frontend/src/pages/planner/ErrorTray.tsx
@@ -7,10 +7,10 @@ import { Spinner } from '../../components/Spinner'
  */
 const NoMessages = ({ open }: { open: boolean }): JSX.Element => {
   return (
-    <div className="flex p-4 mb-4 text-sm text-green-800 border border-green-300 rounded-lg bg-green-50 " role="alert">
-      <svg aria-hidden="true" className="flex-shrink-0 inline w-5 h-5 mr-3" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clipRule="evenodd"></path></svg>
+    <div className="flex p-3 mb-4 text-sm text-green-800 border border-green-300 rounded-lg bg-green-50 " role="alert">
+      <svg aria-hidden="true" className="flex-shrink-0 inline-flex w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clipRule="evenodd"></path></svg>
       <span className="sr-only">Info</span>
-      <div className={`min-w-[14rem] ${open ? '' : 'hidden'} `}>
+      <div className={`min-w-[14rem] ml-2 s${open ? '' : 'hidden'} `}>
         <span className='font-medium'>Felicitaciones!</span> No hay errores o advertencias.
       </div>
     </div>
@@ -24,8 +24,8 @@ const Message = (diag: FlatDiagnostic, key: number, open: boolean): JSX.Element 
   const w = diag.is_warning
 
   return (
-  <div key={key} className={`overflow-hidden flex p-3 text-sm rounded-lg border ${w ? 'text-yellow-700 border-yellow-300 bg-yellow-50' : 'text-red-800 border-red-300 bg-red-50'}`} role="alert">
-    <svg aria-hidden="true" className="flex-shrink-0 inline w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clipRule="evenodd"></path></svg>
+  <div key={key} className={`motion-reduce:transition-none transition-all  overflow-hidden flex p-3 text-sm rounded-lg border ${w ? 'text-yellow-700 border-yellow-300 bg-yellow-50' : 'text-red-800 border-red-300 bg-red-50'}`} role="alert">
+    <svg aria-hidden="true" className="flex-shrink-0 inline-flex w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clipRule="evenodd"></path></svg>
     <span className="sr-only">Info</span>
     <div className={`min-w-[14rem] ml-2 ${open ? '' : 'hidden'} `}>
       <span className={'font-semibold '}>{`${w ? 'Advertencia' : 'Error'}: `}</span>
@@ -51,9 +51,9 @@ const ErrorTray = ({ diagnostics, validating }: { diagnostics: FlatDiagnostic[],
     }
   })
   const messageList: JSX.Element[] = diagnostics.map((diag, index) => Message(diag, index, open || hasError))
+
   return (
-    <div className={`h-[95%] flex relative border-slate-300 border-2 rounded-lg shadow-lg motion-reduce:transition-none transition-all ${hasError || open ? 'w-80 min-w-[20rem]' : 'w-[4.5rem]'}`}>
-      <div className='relative mb-2 overflow-y-auto h-full w-full py-4 rounded-lg  bg-slate-100 z-20 px-3'>
+      <div className={`h-[95%] z-20 flex flex-col relative border-slate-300 border-2 rounded-lg bg-slate-100 shadow-lg mb-2 overflow-y-auto overflow-x-hidden  py-4 px-3  motion-reduce:transition-none transition-all ${hasError || open ? 'w-80 min-w-[20rem]' : 'min-w-[4.5rem]'}`}>
         <div className='flex mb-4 mx-3'>
           <div className='group relative flex'>
             <span className={`fixed z-10 transition-all motion-reduce:transition-none scale-0 ${hasError ? 'group-hover:scale-100' : ''}`}>
@@ -63,18 +63,17 @@ const ErrorTray = ({ diagnostics, validating }: { diagnostics: FlatDiagnostic[],
               </span>
             </span>
             <button className={`${hasError ? 'cursor-not-allowed stroke-slate-400' : 'stroke-current'}`} disabled={hasError} onClick={() => setOpen(prev => !prev)}>
-              <svg className="w-5 h-5 flex" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+              <svg className="w-5 h-5 flex " xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
                   <path d="M2 12h20M2 6h20M2 18h20"></path>
               </svg>
             </button>
           </div>
           {(hasError || open) && <p className="whitespace-nowrap ml-2 text-xl font-semibold text-center h-7 overflow-hidden">Errores y Advertencias </p>}
         </div>
-        <div className="flex flex-col gap-2">
+        <div className="flex flex-col gap-2 w-fit">
             {validating ? <Spinner message={`${hasError || open ? 'Validando...' : ''}`}/> : <>{messageList.length > 0 ? messageList : <NoMessages open={hasError || open}/>}</>}
         </div>
       </div>
-    </div>
   )
 }
 
