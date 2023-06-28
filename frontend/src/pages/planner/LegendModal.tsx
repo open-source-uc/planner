1c22f36f28f6f668197f35dff8b54b15456fe7d7
Shantal Fabri <sfabri1@uc.cl>
2023-06-28 13:58:20 +0000

    fix: modal dimensions

--- frontend/src/pages/planner/LegendModal.tsx
+++ frontend/src/pages/planner/LegendModal.tsx
@@ -31,7 +31,7 @@ const LegendModal = ({ open, onClose }: { open: boolean, onClose: Function }): J
               leaveFrom="opacity-100 translate-y-0 sm:scale-100"
               leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
             >
-            <Dialog.Panel className="w-11/12 px-5 py-6 bg-slate-100 border-slate-300 overflow-hidden max-w-4xl transform  rounded-2xl border-2 text-left align-middle shadow-xl transition-all">
+            <Dialog.Panel className="w-2/3 px-5 py-6 bg-slate-100 border-slate-300 overflow-hidden transform  rounded-2xl border-2 text-left align-middle shadow-xl transition-all">
                 <Dialog.Title as="h3" className="text-2xl font-medium leading-6 text-gray-900 mb-3">
                   Leyenda e Instrucciones
                 </Dialog.Title>
@@ -53,14 +53,14 @@ const LegendModal = ({ open, onClose }: { open: boolean, onClose: Function }): J
                         </Tab>
                       ))}
                     </Tab.List>
-                    <Tab.Panels className="mt-2 h-80 overflow-auto">
-                      <Tab.Panel className='rounded-xl bg-white p-3'>
+                    <Tab.Panels className="mt-2 h-[45vh] overflow-auto">
+                      <Tab.Panel className='rounded-xl bg-white p-3 min-h-full'>
                         <p className="text-sm text-justify text-gray-500">Esta malla curricular es la recomendada y está sujeta a modificaciones conforme a la planificación académica vigente al momento de inscribir cursos.</p>
-                        <p className="text-sm text-justify text-gray-500 my-2">Los requisitos y planificación de los cursos pueden ir variando semestre a semestre. Por lo que PanguiPath puede tener algunas variaciones. La información oficial de los requisitos y planificación de los cursos se encuentra disponible en el catálogo de cursos (http://catalogo.uc.cl/) y el libro de cursos (http://buscacursos.uc.cl/).</p>
+                        <p className="text-sm text-justify text-gray-500 my-8">Los requisitos y planificación de los cursos pueden ir variando semestre a semestre. Por lo que PanguiPath puede tener algunas variaciones. La información oficial de los requisitos y planificación de los cursos se encuentra disponible en el catálogo de cursos (http://catalogo.uc.cl/) y el libro de cursos (http://buscacursos.uc.cl/).</p>
                         <p className="text-sm text-justify text-gray-500">Es responsabilidad del alumno verificar que el avance curricular determinado por PanguiPath sea el correcto. Los Planes de Estudios publicados en Siding corresponden a la información oficial y actualizada.</p>
                       </Tab.Panel>
-                      <Tab.Panel className='rounded-xl bg-white p-3'><Legend /></Tab.Panel>
-                      <Tab.Panel className='rounded-xl bg-white p-3'><Instructions /></Tab.Panel>
+                      <Tab.Panel className='rounded-xl bg-white p-3 min-h-full'><Legend /></Tab.Panel>
+                      <Tab.Panel className='rounded-xl bg-white p-3 min-h-full'><Instructions /></Tab.Panel>
                     </Tab.Panels>
                   </Tab.Group>
                 </div>
