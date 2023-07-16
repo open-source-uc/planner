5eebfdae2985f3be4503943e05869ce5477cd17d
Nicolás Abarca <nicoabarca@uc.cl>
2023-07-16 10:59:46 -0400

    fix: minor style requested changes

--- frontend/src/pages/planner/SavePlanModal.tsx
+++ frontend/src/pages/planner/SavePlanModal.tsx
@@ -33,16 +33,15 @@ const SavePlanModal = ({ isOpen, onClose, savePlan }: { isOpen: boolean, onClose
               leaveFrom="opacity-100 translate-y-0 sm:scale-100"
               leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
             >
-              <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
-                <div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
-                  <div className="sm:flex sm:items-start">
+              <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:max-w-lg">
+                <div className="bg-white pb-4 pt-5 sm:p-6 sm:pb-4">
+                  <div className="sm:flex sm:items-start p-3">
                     <div className="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
                       <Dialog.Title as="h3" className="text-lg font-semibold leading-6 text-gray-900">
                          ¿Cómo quieres llamarle a esta planificación?
                       </Dialog.Title>
                       <div className="mt-2">
-                        <label className="mr-3 my-auto" htmlFor="planName">Nombre de la planificación: </label>
-                        <input className="grow rounded py-1" type="text" id="planName" value={planName} onChange={(e) => { setPlanName(e.target.value) }}/>
+                        <input className="grow rounded py-1 w-full my-2" type="text" id="planName" value={planName} onChange={(e) => { setPlanName(e.target.value) }}/>
                       </div>
                     </div>
                   </div>
