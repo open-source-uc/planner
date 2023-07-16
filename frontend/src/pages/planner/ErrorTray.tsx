2b0cb5ace17d7fdc1c788abc9730d44f8cff0f7f
Diegothx <diego.prudencio@uc.cl>
2023-07-16 22:16:38 +0000

    fix: abilitate minimization of ErrorTray on NoError Message

--- frontend/src/pages/planner/ErrorTray.tsx
+++ frontend/src/pages/planner/ErrorTray.tsx
@@ -14,7 +14,7 @@ const NoMessages = ({ open }: { open: boolean }): JSX.Element => {
     <div className="w-fit flex p-3 mb-4 text-sm text-green-800 border border-green-300 rounded-lg bg-green-50 " role="alert">
       <svg aria-hidden="true" className="flex-shrink-0 inline-flex w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clipRule="evenodd"></path></svg>
       <span className="sr-only">Info</span>
-      <div className={`min-w-[14rem] ml-2 s${open ? '' : 'hidden'} `}>
+      <div className={`min-w-[14rem] ml-2 ${open ? '' : 'hidden'} `}>
         <span className='font-medium'>Felicitaciones!</span> No hay errores o advertencias.
       </div>
     </div>
