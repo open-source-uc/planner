4113324a150fa446bf2e34a1e31faa21f72b2efc
Agustín Covarrubias <agucova@uc.cl>
2023-07-18 20:33:04 +0000

    feat: env modes , logging, fix limiting

---
+++ backend/app/redis.py
@@ -0,0 +1,36 @@
+from __future__ import annotations
+
+from typing import Any
+
+from redis.asyncio import BlockingConnectionPool, Redis
+
+from app.settings import settings
+
+
+def init_redis_pool() -> BlockingConnectionPool:
+    pool: BlockingConnectionPool = BlockingConnectionPool.from_url(  # type: ignore
+        settings.redis_uri,
+        decode_responses=True,
+        encoding="utf-8",
+    )
+    return pool
+
+
+connection_pool = init_redis_pool()
+
+
+# FastAPI dependable for Redis
+async def get_redis():
+    """
+    Get a Redis connection from the pool.
+    """
+    redis: Redis[Any] = Redis(
+        connection_pool=connection_pool,
+        auto_close_connection_pool=False,
+        decode_responses=True,
+        encoding="utf-8",
+    )
+    try:
+        yield redis
+    finally:
+        await redis.close()
