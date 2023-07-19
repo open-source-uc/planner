233c8f5402642a2b72df5f50b20212bc4f132404
Agustín Covarrubias <agucova@uc.cl>
2023-07-19 19:57:36 +0000

    fix: no options available in selector

--- frontend/src/layout/Footer.tsx
+++ frontend/src/layout/Footer.tsx
@@ -1,9 +1,9 @@
 function Footer (): JSX.Element {
-  return (<footer className="mt-3">
+  return (<footer className="mb-2 mt-4 text-gray-600  text-center text-sm">
 
-  <p className="mb-2 text-gray-600  text-center text-sm">Desarrollado por el <a className={'text-blue-700 underline text-sm'} href="https://github.com/open-source-uc/planner/tree/dev#equipo" target="_blank" rel="noreferrer">Equipo Planner</a>📚 con la ayuda de <a className={'text-blue-700 underline text-sm'} href="https://github.com/open-source-uc/planner/blob/dev/contributors.md" target="_blank" rel="noreferrer"> otros contribuidores</a></p>
+  <p className="mb-2">Desarrollado por el <a className={'text-blue-700 underline text-sm'} href="https://github.com/open-source-uc/planner/tree/dev#equipo" target="_blank" rel="noreferrer">Equipo Planner</a> 📚 con la ayuda de <a className={'text-blue-700 underline text-sm'} href="https://github.com/open-source-uc/planner/blob/dev/contributors.md" target="_blank" rel="noreferrer"> otros contribuidores</a></p>
 
-  <p className="mb-3 text-gray-600 text-center text-sm">Este proyecto se encuentra bajo la licencia <a className={'text-blue-700 underline text-sm'} href="https://github.com/open-source-uc/planner/blob/dev/license.md" target="_blank" rel="noreferrer">AGPLv3</a> y es <a className={'text-blue-700 underline text-sm'} href="https://github.com/open-source-uc/planner" target="_blank" rel="noreferrer">código abierto</a>🚀</p>
+  <p className="mb-3">Este proyecto se encuentra bajo la licencia <a className={'text-blue-700 underline text-sm'} href="https://github.com/open-source-uc/planner/blob/dev/license.md" target="_blank" rel="noreferrer">AGPLv3</a> y es <a className={'text-blue-700 underline text-sm'} href="https://github.com/open-source-uc/planner" target="_blank" rel="noreferrer">código abierto</a> 🚀</p>
 </footer>)
 }
 
