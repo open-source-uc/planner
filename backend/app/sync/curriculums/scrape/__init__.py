df012041fe5f445b2b95217fd9e7f97c501b2791
kovaxis <martin.andrigheti@gmail.com>
2023-09-16 15:40:51 +0000

    feat: add minor and title plan scrapers

---
+++ backend/app/sync/curriculums/scrape/__init__.py
@@ -0,0 +1,7 @@
+"""
+Este codigo se encarga de procesar el texto human-readable proveniente de los PDFs de
+plan de estudios y convertirlos en datos computer-readable.
+
+Dado que el codigo es muy especifico para la UC, los comentarios están en español en
+muchos lugares.
+"""
