8ae471ea46f15b7eb25d677de37e8c59efb43d39
kovaxis <martin.andrighetti@gmail.com>
2023-12-30 19:02:36 +0000

    fix: logout in development

--- frontend/src/pages/Logout.tsx
+++ frontend/src/pages/Logout.tsx
@@ -6,10 +6,8 @@ const Logout = (): JSX.Element => {
   console.assert(casURL, 'VITE_CAS_SERVER_URL environment variable not set during build')
 
   // Redirect to SSO logout URL
-  if (!import.meta.env.DEV) {
-    const ssoLogoutURL = new URL('logout', casURL).toString()
-    window.location.href = ssoLogoutURL
-  }
+  const ssoLogoutURL = import.meta.env.DEV ? '/' : new URL('logout', casURL).toString()
+  window.location.href = ssoLogoutURL
 
   return <div className="mx-auto my-auto">
     <h2 className="font-bold text-2xl">
