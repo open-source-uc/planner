082e93f5d30fb62a4ca909aaa1ce33fb40e858fd
Franco Giannoni <franco.giannoni.h@gmail.com>
2023-12-31 17:31:43 -0300

    fix: make vite use also the default env file

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,16 +1,28 @@
-import { defineConfig, loadEnv } from 'vite'
-import { sentryVitePlugin } from "@sentry/vite-plugin";
+import { defineConfig } from 'vite'
+import { sentryVitePlugin } from "@sentry/vite-plugin"
 import react from '@vitejs/plugin-react'
 import eslintPlugin from '@nabla/vite-plugin-eslint'
 import svgr from 'vite-plugin-svgr'
+import dotenv from 'dotenv'
+import fs from 'fs'
+
+function loadEnvWithDefault() {
+  const defaultEnv = dotenv.parse(fs.readFileSync('.env.default'))
+  const env = dotenv.config({ path: ".env" }).parsed
+
+  // Combine default and environment-specific env variables
+  return { ...defaultEnv, ...env }
+}
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command, mode }) => {
-  const env = loadEnv(mode, process.cwd(), '')
+  const env = loadEnvWithDefault()
+
   const backendUrl = env.BACKEND_API_URL
   if (typeof backendUrl !== 'string') {
     throw new Error('BACKEND_API_URL environment variable not set during build')
   }
+
   return {
     build: {
       sourcemap: true,
@@ -28,10 +40,15 @@ export default defineConfig(({ command, mode }) => {
         usePolling: true
       }
     },
-    plugins: [react(), eslintPlugin(), svgr(), sentryVitePlugin({
-      authToken: process.env.SENTRY_AUTH_TOKEN,
-      org: "planner-ing-uc",
-      project: "planner-frontend",
-    })],
+    plugins: [
+      react(), 
+      eslintPlugin(), 
+      svgr(), 
+      sentryVitePlugin({
+        authToken: env.SENTRY_AUTH_TOKEN,
+        org: "planner-ing-uc",
+        project: "planner-frontend",
+      })
+    ],
   }
 })
