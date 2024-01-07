64d51c92c26b13c66485a2e76431bf08463ac054
Franco Giannoni <franco.giannoni.h@gmail.com>
2024-01-07 17:05:38 -0300

    chore: temporary change to test in prepare-deploy branch

--- infra/productive-deploy/update.sh
+++ infra/productive-deploy/update.sh
@@ -7,16 +7,19 @@ REPO_DIR=/opt/planner
 cd "$REPO_DIR"
 
 # Actualizar la rama main del repositorio
-git fetch origin main
+# git fetch origin main
+git fetch origin prepare-deploy
 
 # Obtener el hash del commit local y el remoto
 LOCAL=$(git rev-parse HEAD)
-REMOTE=$(git rev-parse origin/main)
+# REMOTE=$(git rev-parse origin/main)
+REMOTE=$(git rev-parse origin/prepare-deploy)
 
 # Si se detectan cambios, ejecutar run_deploy.sh
 if [ "$LOCAL" != "$REMOTE" ]; then
     echo "Changes detected. Running deploy script..."
-    git checkout origin/main
+    # git checkout origin/main
+    git checkout origin/prepare-deploy
     chmod +x ./run_deploy.sh
     ./run_deploy.sh
 else
