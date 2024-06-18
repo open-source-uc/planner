9f4f952d603ceaedfd80da652f57fb4e08e96125
kovaxis <martin.andrighetti@gmail.com>
2024-06-18 16:54:17 +0000

    arreglar redis

--- backend/app/redis.py
+++ backend/app/redis.py
@@ -1,11 +1,12 @@
 import contextlib
 from typing import Any
 
-from app.settings import settings
 from redis.asyncio import ConnectionPool, Redis
 
+from app.settings import settings
 
-def init_redis_pool() -> ConnectionPool[Any]:
+
+def init_redis_pool() -> ConnectionPool:  # type: ignore
     return ConnectionPool.from_url(  # type: ignore
         settings.redis_uri,
         decode_responses=True,
@@ -13,7 +14,7 @@ def init_redis_pool() -> ConnectionPool[Any]:
     )
 
 
-connection_pool = init_redis_pool()
+connection_pool = init_redis_pool()  # type: ignore
 
 
 @contextlib.asynccontextmanager
@@ -24,7 +25,7 @@ async def get_redis():
     Use with `async with` in order to close the connection at the end of the scope.
     """
     redis: Redis[Any] = Redis(
-        connection_pool=connection_pool,
+        connection_pool=connection_pool,  # type: ignore
         auto_close_connection_pool=False,
         decode_responses=True,
         encoding="utf-8",
