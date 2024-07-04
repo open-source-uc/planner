7fc4bf117a224c9a5b85e281fb65425eb4a3c4af
kovaxis <martin.andrighetti@gmail.com>
2024-07-03 20:51:09 -0400

    mensaje de deprecate

--- frontend/src/layout/Navbar.tsx
+++ frontend/src/layout/Navbar.tsx
@@ -4,106 +4,12 @@ import { memo } from 'react'
 import { hideLogin } from '../utils/featureFlags'
 
 function Navbar (): JSX.Element {
-  const authState = useAuth()
-  const router = useRouter()
-  const loggedIn = authState?.user != null
-  const isMod = authState?.isMod === true
-  const isAdmin = authState?.isAdmin === true
   return (
     <nav className="bg-gray border-slate-200 px-2 sm:px-4 py-2.5 rounded border">
       <div className="container flex flex-wrap items-center justify-between mx-auto">
         <Link to="/" className="self-center">
           <img src="/logo.png" className="h-20" alt="Logo" />
         </Link>
-
-        <div className="hidden w-full md:block md:w-auto" id="navbar-default">
-          <ul className="flex flex-col p-4 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white ">
-            <li>
-              <Link
-                to="/"
-                className={`block py-2 pl-3 pr-40 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 ${
-                  router.state.currentLocation.pathname === '/'
-                    ? 'text-white bg-blue-700 md:bg-transparent md:text-blue-700'
-                    : ''
-                }`}
-              >
-                Inicio
-              </Link>
-            </li>
-            {!isMod && !isAdmin &&
-            <li>
-              <Link
-                to="/planner/new"
-                className={`block py-2 pl-3 pr-40 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 ${
-                  router.state.currentLocation.pathname === '/planner/new'
-                    ? 'text-white bg-blue-700 md:bg-transparent md:text-blue-700'
-                    : ''
-                }`}
-              >
-                Planificar
-              </Link>
-            </li>
-            }
-            {hideLogin
-              ? null
-              : loggedIn
-                ? (
-              <>
-                <li>
-                  {isMod && <Link
-                    to="/mod/users"
-                    className={`block py-2 pl-3 pr-40 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 ${
-                      router.state.currentLocation.pathname === '/mod/users'
-                        ? 'text-white bg-blue-700 md:bg-transparent md:text-blue-700'
-                        : ''
-                    }`}
-                  >
-                    Buscar mallas
-                  </Link>
-                  }{isAdmin && <Link
-                    to="/admin/mods"
-                    className={`block py-2 pl-3 pr-40 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 ${
-                      router.state.currentLocation.pathname === '/admin/mods'
-                        ? 'text-white bg-blue-700 md:bg-transparent md:text-blue-700'
-                        : ''
-                    }`}
-                  >
-                    Gestionar Moderadores
-                  </Link>
-                  }
-                  {!isMod && !isAdmin && <Link
-                    to="/user"
-                    className={`block py-2 pl-3 pr-40 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 ${
-                      router.state.currentLocation.pathname === '/user'
-                        ? 'text-white bg-blue-700 md:bg-transparent md:text-blue-700'
-                        : ''
-                    }`}
-                  >
-                    Mis mallas
-                  </Link>}
-                </li>
-                <li>
-                  <Link
-                    to="/logout"
-                    className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0"
-                  >
-                    Cerrar sesión
-                  </Link>
-                </li>
-              </>
-                  )
-                : (
-              <li>
-                <a
-                  href="/api/user/login"
-                  className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0"
-                >
-                  Iniciar sesión
-                </a>
-              </li>
-                  )}
-          </ul>
-        </div>
       </div>
     </nav>
 
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -15,95 +15,15 @@ const AlertMobileUssage = (): JSX.Element => {
 }
 
 const Home = (): JSX.Element => {
-  const authState = useAuth()
-  const [disclaimerIsOpen, setDisclaimerIsOpen] = useState(true)
-
-  const loggedIn = authState?.user != null
-  const isMod = authState?.isMod === true
-  const isAdmin = authState?.isAdmin === true
   return (
     <div className="bg-gradient-to-b from-blue-500 h-full">
-      <AlertModal
-        title={'Importante'}
-        isOpen={disclaimerIsOpen}
-        close={() => {
-          setDisclaimerIsOpen(false)
-        }}
-        disclaimer={true}
-      >
-        <div className="prose text-md">
-          <p className="mb-0">
-            Planner puede cometer errores.
-          </p>
-          <ul className="list-disc pl-5 mt-0">
-            <li>
-              Las mallas validadas pueden tener errores.
-              <ul>
-                <li>Revisa manualmente que la malla haga sentido.</li>
-                <li>Recuerda que SIDING es la referencia oficial.</li>
-              </ul>
-            </li>
-            {
-              hideLogin &&
-                (<li>
-                Por ahora, el <span className='font-bold'>login UC no está funcionando</span>, así que
-                debes <a href="https://github.com/open-source-uc/planner/pull/276#issue-1808769979" target="_blank" rel="noopener noreferrer">
-                  ingresar tus cursos manualmente en el modo invitado
-                </a>.
-              </li>)
-            }
-          </ul>
-        </div>
-      </AlertModal>
       <div className="max-w-3xl mx-auto mt-8 center">
-        <AlertMobileUssage />
         <h1 className="text-white text-center text-4xl mb-8 font-medium">
-          Bienvenido al Nuevo Planner, el lugar donde puedes planificar tu
-          carrera universitaria
+          El Nuevo Planner se ha cambiado de hogar 🏠
+          <br/>
+          Dirígete a <a href="https://mallas.ing.uc.cl">mallas.ing.uc.cl</a>
         </h1>
-        <div className="flex justify-evenly">
-          {
-          hideLogin
-            ? (
-            <Link to="/planner/new">
-            <button className="rounded-lg py-2 px-4 bg-blue-800 text-white">
-              Planificar Malla
-            </button>
-          </Link>
-              )
-            : (loggedIn
-                ? <>{isMod && <Link to="/mod/users">
-                    <button className="rounded-lg py-2 px-4 bg-blue-800 text-white">
-                      Buscar Mallas de estudiantes
-                    </button>
-                  </Link>}
-                  {isAdmin && <Link to="/admin/mods">
-                    <button className="rounded-lg py-2 px-4 bg-blue-800 text-white">
-                      Gestionar Moderadores
-                    </button>
-                  </Link>}
-                  {!isMod && !isAdmin && <Link to="/user">
-                      <button className="rounded-lg py-2 px-4 bg-blue-800 text-white">
-                        Mis mallas
-                      </button>
-                    </Link>
-                  }</>
-                : (<>
-              <a href="/api/user/login">
-                <button className="rounded-lg py-2 px-4 bg-blue-800 text-white">
-                  Iniciar sesión
-                </button>
-              </a>
-              <Link to="/planner/new">
-                <button className="rounded-lg py-2 px-4 bg-blue-800 text-white">
-                  Continuar como invitado
-                </button>
-              </Link>
-              </>
-                  ))}
-        </div>
       </div>
-      <img className="p-2 mx-auto max-w-2xl w-full aspect-auto" alt="demo" src={demoGif}></img>
     </div>
   )
 }
