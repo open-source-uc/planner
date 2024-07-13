be5788d6797c498d2c491fcb5fd98716eb1af7aa
Diego Prudencio Cerfogli <37160496+Diegothx@users.noreply.github.com>
2024-07-13 19:52:23 -0400

    Update OpenAPI.ts

--- frontend/src/client/core/OpenAPI.ts
+++ frontend/src/client/core/OpenAPI.ts
@@ -19,7 +19,7 @@ export type OpenAPIConfig = {
 };
 
 export const OpenAPI: OpenAPIConfig = {
-    BASE: 'http://localhost:8000',
+    BASE: '/api',
     VERSION: '0.1.0',
     WITH_CREDENTIALS: false,
     CREDENTIALS: 'include',
